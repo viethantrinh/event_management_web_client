bda34f96c36c542771a4291202b6cd4132b46049
viethantrinh <hntrnn12@gmail.com>
2025-08-20 13:04:30 +0800

    f

--- src/app/utils/app.constants.ts
+++ src/app/utils/app.constants.ts
@@ -1,4 +1,4 @@
 const BASE_API_LOCAL_URL = "http://localhost:8080/api"
 const BASE_API_REMOTE_URL = "https://event-management-server-5a10dec6cc20.herokuapp.com/api"
-export const BASE_API_URL = BASE_API_LOCAL_URL
+export const BASE_API_URL = BASE_API_REMOTE_URL
 export const AUTH_TOKEN = 'AUTH_TOKEN'
